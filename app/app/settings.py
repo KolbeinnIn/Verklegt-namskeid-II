28ce66203cfb6294d59121c28bd525b4e3d0f56c
Bjarki Jónsson <bjarkithorjonsson@gmail.com>
2020-05-04 16:24:14 +0000

    Database

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xdjwfuhe',
+        'USER': 'xdjwfuhe',
+        'PASSWORD': 'uiMRXW8vVlzQgpbrXt1Xz-nZ_s_lTBDy',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
