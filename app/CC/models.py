d1a1b2d13cc2a662d954873be901ed93c852e42d
Bjarki Jónsson <bjarkithorjonsson@gmail.com>
2020-05-04 17:19:52 +0000

    added model classes

--- app/CC/models.py
+++ app/CC/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
+    URL_keyword = models.CharField(max_length=255, blank=True)
+    parent_id = models.ForeignKey('self', on_delete=models.DO_NOTHING)
+
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    relative_path = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    manufacturer = models.CharField(max_length=255, blank=True)
+    url = models.CharField(max_length=255, blank=True)
+    P_EAN = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+    discount = models.FloatField(default=0)
+    description = models.CharField(max_length=1024, blank=True)
+    status = models.BooleanField(default=True)
+    category_id = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    image_something = models.ForeignKey(Image, on_delete=models.DO_NOTHING)
