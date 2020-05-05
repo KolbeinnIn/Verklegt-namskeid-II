f5cd26eb30780136781fabcaafd8d2146ad20816
Bjarki Jónsson <bjarkithorjonsson@gmail.com>
2020-05-05 12:14:53 +0000

    database drasl 2

--- app/CC/models.py
+++ app/CC/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     status = models.BooleanField(default=True, blank=True)
     URL_keyword = models.CharField(max_length=255, blank=True)
-    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, blank=True, null= True)
+    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, blank=True, null=True)
 
 
 class Image(models.Model):
@@ -23,5 +23,7 @@ class Product(models.Model):
     discount = models.FloatField(default=0, blank=True)
     description = models.CharField(max_length=1024, blank=True)
     status = models.BooleanField(default=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, blank=True, null= True)
-    image = models.ForeignKey(Image, on_delete=models.DO_NOTHING, blank=True, null= True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, blank=True, null=True)
+    image = models.ForeignKey(Image, on_delete=models.DO_NOTHING, blank=True, null=True)
+    def __str__(self):
+        return self.name
