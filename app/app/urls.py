a50c39ab1302647b7bbbd35f90e16eadfa54ea5f
Kalliarolunni <gustigaur@gmail.com>
2020-05-06 11:21:43 +0000

    Update urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('', include("CC.urls")),
     path('products/', include("product_list.urls")),
     path('products/<int:id>', include("product_list.urls")),
+    path('product/', include("product.urls")),
     path('admin/', admin.site.urls),
 ]
