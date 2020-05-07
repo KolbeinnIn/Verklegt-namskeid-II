2ccb4e5924d454bccf1c87a5c3e0d37d7816970a
Bjarki Jónsson <bjarkithorjonsson@gmail.com>
2020-05-07 16:42:01 +0000

    breyta forms

--- app/staff/views.py
+++ app/staff/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from staff.forms.product_form import ProductCreateForm
+from staff.forms.product_form import ProductCreateForm,CategoryCreateForm
 from CC.models import Image
 
 # Create your views here.
@@ -7,6 +7,7 @@ def create_product(request):
     if request.method == 'POST':
         form = ProductCreateForm(data=request.POST)
         if form.is_valid():
+            #print(form.cleaned_data['name'])
             product = form.save()
             image = Image(name="Placeholder", relative_path=request.POST['image'])
             image.save()
@@ -16,5 +17,17 @@ def create_product(request):
     else:
         form = ProductCreateForm()
     return render(request, "staff/create_product.html", {
+        'form': form
+    })
+
+def create_category(request):
+    if request.method == 'POST':
+        form = CategoryCreateForm(data=request.POST)
+        if form.is_valid():
+            category = form.save()
+            return redirect('/')
+    else:
+        form = CategoryCreateForm()
+    return render(request, "staff/create_category.html", {
         'form': form
     })
