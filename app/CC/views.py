f46ab9a79c4afa5d0be91babad8f99ac7f0ba3af
Haraldur-Dadi <halli.heima@gmail.com>
2020-05-08 16:37:05 +0000

    Vinna í forsíðunni
    Bætti við 2 banner, einn sem hægt væri að nota fyrir promotion og annað fyrir vinsælar vörur

--- app/CC/views.py
+++ app/CC/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from CC.models import Product
 
 
 def index(request):
-    return render(request, "CC/index.html", context={"listi": {999: "vara1", 2: "vara2", 3: "vara3", 4: "vara4", }})
-
-
-
-
+    return render(request,
+                  "CC/index.html",
+                  context={
+                      "prod_list": Product.objects.all()[:6]
+                  })
