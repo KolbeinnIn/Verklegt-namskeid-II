c6801e5001dbaaba4679a02cc5c893c09313f070
Haraldur-Dadi <halli.heima@gmail.com>
2020-05-08 17:31:57 +0000

    Kláraði forsíðuna
    bætti við svæði fyrir "nýjar vörur"

--- app/CC/views.py
+++ app/CC/views.py
@@ -3,8 +3,16 @@ from CC.models import Product
 
 
 def index(request):
+    popular_product_list = []
+    new_product_list = []
+    if len(Product.objects.all()) > 6:
+        popular_product_list = Product.objects.all()[:6]
+    if len(Product.objects.all()) > 10:
+        new_product_list = Product.objects.all()[len(Product.objects.all()) - 6:]
+
     return render(request,
                   "CC/index.html",
                   context={
-                      "prod_list": Product.objects.all()[:6]
+                      "popular_product_list": popular_product_list,
+                      "new_product_list": new_product_list
                   })
