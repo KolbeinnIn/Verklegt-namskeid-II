05ce374d83f3621c047a7f0e104ab568098d2083
Haraldur-Dadi <halli.heima@gmail.com>
2020-05-11 13:11:09 +0000

    staff login is comlete :)

--- app/staff/views.py
+++ app/staff/views.py
@@ -1,40 +1,48 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from staff.forms.product_form import ProductCreateForm, CategoryCreateForm
 from CC.models import Image, Category
-from staff.forms.staff_login_form import StaffLoginForm
 
 
-def login_view(request):
-    if request.method == 'Post':
-        form = StaffLoginForm(date=request.POST)
-        if form.is_valid():
-            username = request.get("username")
-            password = request.get("password")
-            user = authenticate(username=username, password=password)
-            if user is not None:
+def login_staff_view(request):
+    logout(request)
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_staff or user.is_superuser:
+                login(request, user)
                 return redirect("dashboard")
-
-    return render(request, "staff/login.html", {
-        'form': StaffLoginForm()
-    })
+            else:
+                form = AuthenticationForm(request.POST)
+                return render(request, "staff/login.html", {'form': form})
+        else:
+            form = AuthenticationForm(request.POST)
+            return render(request, "staff/login.html", {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, "staff/login.html", {'form': form})
 
 
 def create_product(request):
-    if request.method == 'POST':
-        form = ProductCreateForm(data=request.POST)
-        if form.is_valid():
-            product = form.save()
-            product.check_url()
-            image = Image(name="Placeholder", relative_path=request.POST['image'])
-            image.save()
-            product.image.add(image)
-            return redirect('/')
+    if request.user.is_staff or request.user.is_superuser:
+        if request.method == 'POST':
+            form = ProductCreateForm(data=request.POST)
+            if form.is_valid():
+                product = form.save()
+                product.check_url()
+                image = Image(name="Placeholder", relative_path=request.POST['image'])
+                image.save()
+                product.image.add(image)
+                return redirect('/')
+        else:
+            form = ProductCreateForm()
+        return render(request, "staff/create_product.html", {'form': form})
     else:
-        form = ProductCreateForm()
-    return render(request, "staff/create_product.html", {
-        'form': form
-    })
+        return redirect("login_staff")
+
 
 def create_category(request):
     if request.method == 'POST':
