59e6b635991f18d3fd6f3d28d43be51ff8bee75d
KolbeinnIn <kolbeinningo@simnet.is>
2020-05-13 16:52:43 +0000

    lala

--- app/product/views.py
+++ app/product/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from CC.models import Product
+from django import template
+
+
+def lmao(prod):
+    print(prod.name)
 
 
 def index(request, prod_url):
-    return render(request, "product/index.html", context={'product': get_object_or_404(Product, URL_keyword=prod_url)})
+    return render(request, "product/index.html",
+                  context={
+                      'product': get_object_or_404(Product, URL_keyword=prod_url),
+                      'some_func': lmao
+                  })
 
 
 def add_item_to_cart(request):
