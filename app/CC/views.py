a9552e7e81ce94de0fb40e6955f3e833b1398588
Haraldur-Dadi <halli.heima@gmail.com>
2020-05-13 17:43:44 +0000

    Uppfærði hvering vara er sett í körfu
    Bætti við að hægt er að bæta quantity á vöru, færði til vegna conflicts í url mapping

--- app/CC/views.py
+++ app/CC/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from CC.models import Product
-
+from django.shortcuts import render, redirect
+from CC.models import Product, Cart, CartItem, Shipping, Category, Order
+from user.models import profile_info
 
 def index(request):
     popular_product_list = []
@@ -17,3 +17,47 @@ def index(request):
                       "popular_product_list": popular_product_list,
                       "new_product_list": new_product_list
                   })
+
+
+def add_item_to_cart(request):
+    # Get parameters from request
+    prod_url = request.GET.get("prod")
+    quantity = request.GET.get("quantity")
+    # Get product from parameters
+    product = Product.objects.filter(URL_keyword=prod_url).first()
+    if product:
+        # if the user is logged in we will store the cart in the database
+        if request.user.is_authenticated:
+            person_info = profile_info.objects.filter(user=request.user).first()
+            cart = Cart.objects.filter(person_info=person_info).last()
+
+            # If cart doesnt exists we need to create it
+            if not cart:
+                cart = Cart()
+                cart.person_info = person_info
+                cart.save()
+            else:
+                order = Order.objects.filter(cart=cart).first()
+                # Check if cart been made into order, then we need to assign new one
+                if order:
+                    cart = Cart()
+                    cart.person_info = person_info
+                    cart.save()
+
+            cart_item = CartItem.objects.filter(product=product, cart=cart).first()
+            # If the item is not already in this cart we need to create it
+            if not cart_item:
+                cart_item = CartItem()
+                cart_item.product = product
+                cart_item.cart = cart
+            # Update parameters of cart item and save it
+            cart_item.quantity += int(quantity)
+            cart_item.unit_price = product.total
+            cart_item.save()
+        else:
+            # add to cookies
+            a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ég er að brjálast"
+        return redirect("/vara/" + prod_url)
+    else:
+        # Item dosnt exist so we send them to the front page
+        return redirect('CC-index')
