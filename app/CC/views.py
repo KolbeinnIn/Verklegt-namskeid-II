bc461c7522a85f3e3908a8699193bdb126b9b859
Haraldur-Dadi <halli.heima@gmail.com>
2020-05-13 19:21:54 +0000

    update cart
    Núna geymir það líka cart fyrir þá sem eru ekki skráðir inn.
    Breytti carItem, geymir nú upplýsingar um vöru en ekki vöruna sjálfa

--- app/CC/views.py
+++ app/CC/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from CC.models import Product, Cart, CartItem, Shipping, Category, Order
 from user.models import profile_info
 
+
 def index(request):
     popular_product_list = []
     new_product_list = []
@@ -21,43 +22,70 @@ def index(request):
 
 def add_item_to_cart(request):
     # Get parameters from request
-    prod_url = request.GET.get("prod")
+    prod_id = request.GET.get("prod")
     quantity = request.GET.get("quantity")
+
     # Get product from parameters
-    product = Product.objects.filter(URL_keyword=prod_url).first()
+    product = Product.objects.filter(id=prod_id).first()
+
     if product:
-        # if the user is logged in we will store the cart in the database
-        if request.user.is_authenticated:
-            person_info = profile_info.objects.filter(user=request.user).first()
-            cart = Cart.objects.filter(person_info=person_info).last()
+        cart = create_cart(request)
+        cart_item = CartItem.objects.filter(prod_id=product.id, cart=cart).first()
 
-            # If cart doesnt exists we need to create it
-            if not cart:
-                cart = Cart()
-                cart.person_info = person_info
-                cart.save()
-            else:
-                order = Order.objects.filter(cart=cart).first()
-                # Check if cart been made into order, then we need to assign new one
-                if order:
-                    cart = Cart()
-                    cart.person_info = person_info
-                    cart.save()
-
-            cart_item = CartItem.objects.filter(product=product, cart=cart).first()
-            # If the item is not already in this cart we need to create it
-            if not cart_item:
-                cart_item = CartItem()
-                cart_item.product = product
-                cart_item.cart = cart
-            # Update parameters of cart item and save it
-            cart_item.quantity += int(quantity)
-            cart_item.unit_price = product.total
-            cart_item.save()
+        # If the item is not already in this cart we need to create it
+        if not cart_item:
+            cart_item = CartItem()
+            cart_item.prod_id = int(product.id)
+            cart_item.quantity = quantity
+            cart_item.cart = cart
         else:
-            # add to cookies
-            a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ég er að brjálast"
-        return redirect("/vara/" + prod_url)
+            cart_item.quantity += int(quantity)
+
+        # Update parameters of cart item and save it
+        cart_item.prod_name = product.name
+        cart_item.unit_price = product.total
+        cart_item.save()
+        return redirect("/vara/" + product.URL_keyword)
     else:
         # Item dosnt exist so we send them to the front page
         return redirect('CC-index')
+
+
+def create_cart(request):
+    # if the user is logged in we will store the cart with the user
+    person_info = None
+    # if user is not logged in we need to store the cart with the session id/key
+    session_id = None
+
+    if request.user.is_authenticated:
+        # get the person info of user and cart associated with it
+        person_info = profile_info.objects.filter(user=request.user).first()
+        cart = Cart.objects.filter(person_info=person_info).last()
+    else:
+        # if user is not in and there is not a session we must create one
+        if not request.session.exists(request.session.session_key):
+            request.session.create()
+        # get the session_id and cart associated with it
+        session_id = request.session.session_key
+        cart = Cart.objects.filter(session_id=session_id).first()
+
+    # If cart that we tried to get doesn't exists we need to create it
+    if not cart:
+        cart = Cart()
+        if person_info:
+            cart.person_info = person_info
+        else:
+            cart.session_id = session_id
+        cart.save()
+    # Check if cart been made into order, then we need to assign new one
+    else:
+        order = Order.objects.filter(cart=cart).first()
+        if order:
+            cart = Cart()
+            if person_info:
+                cart.person_info = person_info
+            else:
+                cart.session_id = session_id
+            cart.save()
+
+    return cart
