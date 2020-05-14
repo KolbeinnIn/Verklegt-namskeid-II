dad06a54d8bd73b6bd8abdb50b7e46a916fe1258
Haraldur-Dadi <halli.heima@gmail.com>
2020-05-14 15:49:53 +0000

    Bæta við update-quantity og delete-from-cart

--- app/CC/urls.py
+++ app/CC/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="CC-index"),
     path('add-to-cart', views.add_item_to_cart, name="add-to-cart"),
+    path('change-quantity', views.change_quantity, name="change-quantity"),
+    path('delete-from-cart', views.delete_from_cart, name="delete-from-cart"),
 ]
